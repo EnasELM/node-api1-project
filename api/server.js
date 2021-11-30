d1a878576c56e1d72f63d80c6a2eee58fbb73ba0
Enas <enaselmahdwi@gmail.com>
2021-11-29 20:30:59 -0700

    updatr

--- api/server.js
+++ api/server.js
@@ -103,5 +103,10 @@ server.get("/api/users/:id", (req, res) => {
       });
     });
 });
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message:'not found'
+    })
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
